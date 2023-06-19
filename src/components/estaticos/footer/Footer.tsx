f8162b525ac5ad4fec20d4984e87d918a65b9336
FSFbot <felipe.sfreitas14@gmail.com>
2023-06-19 20:33:57 -0300

    Atualização da navbar e footer

--- src/components/estaticos/footer/Footer.tsx
+++ src/components/estaticos/footer/Footer.tsx
@@ -2,44 +2,70 @@ import React from "react";
 import InstagramIcon from '@material-ui/icons/Instagram';
 import FacebookIcon from '@material-ui/icons/Facebook';
 import LinkedInIcon from '@material-ui/icons/LinkedIn';
-import { Typography, Grid } from '@material-ui/core';
-import { Box } from '@mui/material';
+import GitHubIcon from '@mui/icons-material/GitHub';
+import WhatsAppIcon from '@mui/icons-material/WhatsApp';
+import "./Footer.css";
+import { Box, Grid, Typography } from "@material-ui/core";
 
 function Footer() {
-    return (
-        <>
-            <Grid container direction="row" justifyContent="center" alignItems="center">
-                <Grid alignItems="center" item xs={12}>
-                    <Box style={{ backgroundColor: "#3F51B5", height: "120px" }}>
-                        <Box paddingTop={1} display="flex" alignItems="center" justifyContent="center">
-                            <Typography variant="h5" align="center" gutterBottom style={{ color: "white" }}>Siga-nos nas redes sociais </Typography>
-                        </Box>
-                        <Box display="flex" alignItems="center" justifyContent="center">
-                            <a href="https://www.facebook.com/generationbrasil" target="_blank" rel="noopener noreferrer">
-                                <FacebookIcon style={{ fontSize: 60, color: "white" }} />
-                            </a>
-                            <a href="https://www.instagram.com/generationbrasil/" target="_blank" rel="noopener noreferrer">
-                                <InstagramIcon style={{ fontSize: 60, color: "white" }} />
-                            </a>
-                            <a href="https://www.linkedin.com/school/generationbrasil/" target="_blank" rel="noopener noreferrer">
-                                <LinkedInIcon style={{ fontSize: 60, color: "white" }} />
-                            </a>
-                        </Box>
-                    </Box>
-                    <Box style={{ backgroundColor: "#303F9F", height: "60px" }}>
-                        <Box paddingTop={1}>
-                            <Typography variant="subtitle2" align="center" gutterBottom style={{ color: "white" }} >© 2020 Copyright:</Typography>
-                        </Box>
-                        <Box>
-                            <a target="_blank" href="https://brasil.generation.org" rel="noopener noreferrer">
-                                <Typography variant="subtitle2" gutterBottom style={{ color: "white" }} align="center">brasil.generation.org</Typography>
-                            </a>
-                        </Box>
-                    </Box>
-                </Grid>
-            </Grid>
-        </>
-    )
+  return (
+    <>
+      <Grid container direction="row" justifyContent="center" alignItems="center" style={{backgroundColor:"9ef5cf" }}>
+        <Grid container justifyContent="space-between" >
+          <Box>
+            <img src="https://st3.depositphotos.com/31445094/36396/v/600/depositphotos_363967856-stock-illustration-green-leaf-ecology-nature-element.jpg" alt="imagem de uma planta generica" style={{width:"20em"}}/>
+          </Box>
+          <Box>
+            <img src="https://st3.depositphotos.com/31445094/36396/v/600/depositphotos_363967856-stock-illustration-green-leaf-ecology-nature-element.jpg" alt="imagem de uma planta generica" style={{width:"20em"}}/>
+          </Box>
+          <Box>
+            <img src="https://st3.depositphotos.com/31445094/36396/v/600/depositphotos_363967856-stock-illustration-green-leaf-ecology-nature-element.jpg" alt="imagem de uma planta generica" style={{width:"20em"}}/>
+          </Box>
+        </Grid>
+        <Grid>
+          <img src="https://img.freepik.com/icones-gratis/plantar_318-247366.jpg?q=10&h=200" alt="imagem de uma planta generica" />
+        <Typography style={{fontSize: "3em"}}> Entre em contato conosco</Typography>
+        </Grid>
+        <Grid alignItems="center" item xs={12}>
+          <Box paddingTop={1} display={"flex"} alignItems={"center"}justifyContent={"center"} style={{backgroundColor:"#9ef5cf"}}>
+            <Typography variant="h5"align="center"gutterBottom style={{ color: "2a2a2a" }}>
+              Siga-nos nas redes sociais{" "}
+            </Typography>
+            <Box display="flex" alignItems="center" justifyContent="center">
+              <a href="https://www.facebook.com/generationbrasil"target="_blank">
+                <FacebookIcon style={{ fontSize: 60, color: "#2a2a2a" }} />
+              </a>
+              <a href="https://www.instagram.com/generationbrasil/"target="_blank">
+                <InstagramIcon style={{ fontSize: 60, color: "#2a2a2a" }} />
+              </a>
+              <a href="https://www.linkedin.com/school/generationbrasil/"target="_blank">
+                <LinkedInIcon style={{ fontSize: 60, color: "#2a2a2a" }} />
+              </a>
+              <a href="https://www.linkedin.com/school/generationbrasil/"target="_blank">
+                <GitHubIcon style={{ fontSize: 60, color: "2a2a2a" }} />
+              </a>
+              <a href="https://www.linkedin.com/school/generationbrasil/"target="_blank">
+                <WhatsAppIcon style={{ fontSize: 60, color: "2a2a2a" }} />
+              </a>
+            </Box>
+          </Box>
+          <Box style={{ backgroundColor: "#9ef5cf", height: "60px" }}>
+            <Box paddingTop={1}>
+              <Typography variant="subtitle2" align="center" gutterBottom style={{ color: "2a2a2a" }}>
+                © 2023 Copyright:
+              </Typography>
+            </Box>
+            <Box>
+              <a target="_blank" href="https://brasil.generation.org">
+                <Typography variant="subtitle2"gutterBottom style={{ color: "2a2a2a" }} align="center">
+                  Grupo 4 
+                </Typography>
+              </a>
+            </Box>
+          </Box>
+        </Grid>
+      </Grid>
+    </>
+  );
 }
-
 export default Footer;
--- src/components/estaticos/navbar/Navbar.tsx
+++ src/components/estaticos/navbar/Navbar.tsx
@@ -7,41 +7,62 @@ import { Box } from '@mui/material';
 function Navbar() {
     return (
         <>
-            <AppBar position="static">
+            <AppBar position="static" style={{background: "#9ef5cf"}}>
                 <Toolbar variant="dense">
                     <Box className='cursor'>
-                        <Typography variant="h5" color="inherit">
-                            BlogPessoal
+                        <Typography variant="h5" style={{color: "#2a2a2a"}}>
+                            Hortykut 
                         </Typography>
                     </Box>
 
                     <Box display="flex" justifyContent="start">
                         <Box mx={1} className='cursor'>
-                            <Typography variant="h6" color="inherit">
-                                home
+                            <Typography variant="h6" style={{color: "#2a2a2a"}}>
+                                Venda
                             </Typography>
                         </Box>
                         <Box mx={1} className='cursor'>
-                            <Typography variant="h6" color="inherit">
-                                sobre
+                            <Typography variant="h6" style={{color: "#2a2a2a"}}>
+                                Compra
                             </Typography>
                         </Box>
                         <Box mx={1} className='cursor'>
-                            <Typography variant="h6" color="inherit">
-                                temas
+                            <Typography variant="h6" style={{color: "#2a2a2a"}}>
+                                transporte
                             </Typography>
                         </Box>
                         <Box mx={1} className='cursor'>
-                            <Typography variant="h6" color="inherit">
-                                cadastrar tema
+                            <Typography variant="h6" style={{color: "#2a2a2a"}}>
+                                cursos
+                            </Typography>
+                        </Box>
+                        <Box mx={1} className='cursor'>
+                            <Typography variant="h6" style={{color: "#2a2a2a"}}>
+                                planos
+                            </Typography>
+                        </Box>
+                        <Box mx={1} className='cursor'>
+                            <Typography variant="h6" style={{color: "#2a2a2a"}}>
+                                Sobre nós
+                            </Typography>
+                        </Box>
+                        <Box mx={1} className='cursor'>
+                            <Typography variant="h6" style={{color: "#2a2a2a"}}>
+                                suporte
+                            </Typography>
+                        </Box>
+                        <Box mx={1} className='cursor'>
+                            <Typography variant="h6" style={{color: "#2a2a2a"}}>
+                                Faça Parte
                             </Typography>
                         </Box>
                         <Link to='/login' className='text-decorator-none'>
                             <Box mx={1} className='cursor'>
-                                <Typography variant="h6" color="inherit">
+                                <Typography variant="h6" style={{color: "#2a2a2a"}}>
                                     logout
                                 </Typography>
                             </Box>
+                            
                         </Link>
                     </Box>
                     
