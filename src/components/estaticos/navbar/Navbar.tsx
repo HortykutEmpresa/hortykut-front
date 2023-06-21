a60c1b83e12b74525a2ec5297ec608163ea596a2
unknown <kellynamaral@yahoo.com>
2023-06-21 12:56:50 -0300

    implementação do login

--- src/components/estaticos/navbar/Navbar.tsx
+++ src/components/estaticos/navbar/Navbar.tsx
@@ -7,65 +7,70 @@ import { Box } from '@mui/material';
 function Navbar() {
     return (
         <>
-            <AppBar position="static" style={{background: "#9ef5cf"}}>
+            <AppBar position="static" style={{ background: "#9ef5cf" }}>
                 <Toolbar variant="dense">
                     <Box className='cursor'>
-                        <Typography variant="h5" style={{color: "#2a2a2a"}}>
-                            Hortykut 
+                        <Typography variant="h5" style={{ color: "#2a2a2a" }}>
+                            Hortykut
                         </Typography>
                     </Box>
-
-                    <Box display="flex" justifyContent="start">
+                    <Box display="flex " justifyContent="start">
                         <Box mx={1} className='cursor'>
-                            <Typography variant="h6" style={{color: "#2a2a2a"}}>
-                                Venda
+                            <Typography variant="h6" style={{ color: "#2a2a2a" }}>
+                                Home
                             </Typography>
                         </Box>
-                        <Box mx={1} className='cursor'>
-                            <Typography variant="h6" style={{color: "#2a2a2a"}}>
-                                Compra
-                            </Typography>
-                        </Box>
-                        <Box mx={1} className='cursor'>
-                            <Typography variant="h6" style={{color: "#2a2a2a"}}>
-                                transporte
-                            </Typography>
-                        </Box>
-                        <Box mx={1} className='cursor'>
-                            <Typography variant="h6" style={{color: "#2a2a2a"}}>
-                                cursos
-                            </Typography>
-                        </Box>
-                        <Box mx={1} className='cursor'>
-                            <Typography variant="h6" style={{color: "#2a2a2a"}}>
-                                planos
-                            </Typography>
-                        </Box>
-                        <Box mx={1} className='cursor'>
-                            <Typography variant="h6" style={{color: "#2a2a2a"}}>
-                                Sobre nós
-                            </Typography>
-                        </Box>
-                        <Box mx={1} className='cursor'>
-                            <Typography variant="h6" style={{color: "#2a2a2a"}}>
-                                suporte
-                            </Typography>
-                        </Box>
-                        <Box mx={1} className='cursor'>
-                            <Typography variant="h6" style={{color: "#2a2a2a"}}>
-                                Faça Parte
-                            </Typography>
-                        </Box>
-                        <Link to='/login' className='text-decorator-none'>
+                        <Box display="flex" justifyContent="start">
                             <Box mx={1} className='cursor'>
-                                <Typography variant="h6" style={{color: "#2a2a2a"}}>
-                                    logout
+                                <Typography variant="h6" style={{ color: "#2a2a2a" }}>
+                                    Venda
                                 </Typography>
                             </Box>
-                            
-                        </Link>
+                            <Box mx={1} className='cursor'>
+                                <Typography variant="h6" style={{ color: "#2a2a2a" }}>
+                                    Compra
+                                </Typography>
+                            </Box>
+                            <Box mx={1} className='cursor'>
+                                <Typography variant="h6" style={{ color: "#2a2a2a" }}>
+                                    transporte
+                                </Typography>
+                            </Box>
+                            <Box mx={1} className='cursor'>
+                                <Typography variant="h6" style={{ color: "#2a2a2a" }}>
+                                    cursos
+                                </Typography>
+                            </Box>
+                            <Box mx={1} className='cursor'>
+                                <Typography variant="h6" style={{ color: "#2a2a2a" }}>
+                                    planos
+                                </Typography>
+                            </Box>
+                            <Box mx={1} className='cursor'>
+                                <Typography variant="h6" style={{ color: "#2a2a2a" }}>
+                                    Sobre nós
+                                </Typography>
+                            </Box>
+                            <Box mx={1} className='cursor'>
+                                <Typography variant="h6" style={{ color: "#2a2a2a" }}>
+                                    suporte
+                                </Typography>
+                            </Box>
+                            <Box mx={1} className='cursor'>
+                                <Typography variant="h6" style={{ color: "#2a2a2a" }}>
+                                    Faça Parte
+                                </Typography>
+                            </Box>
+                            <Link to='/login' className='text-decorator-none'>
+                                <Box mx={1} className='cursor'>
+                                    <Typography variant="h6" style={{ color: "#2a2a2a" }}>
+                                        logout
+                                    </Typography>
+                                </Box>
+
+                            </Link>
+                        </Box>
                     </Box>
-                    
                 </Toolbar>
             </AppBar>
         </>
