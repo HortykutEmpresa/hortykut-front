b83838a864ba64d77af3384d0752484e5bdb05c9
Sofia Marchetti <113708530+sofiamarchetti@users.noreply.github.com>
2023-06-21 13:07:34 -0300

    Atualização CSS Footer e Navbar

--- src/components/estaticos/navbar/Navbar.tsx
+++ src/components/estaticos/navbar/Navbar.tsx
@@ -7,65 +7,63 @@ import { Box } from '@mui/material';
 function Navbar() {
     return (
         <>
-            <AppBar position="static" style={{background: "#9ef5cf"}}>
-                <Toolbar variant="dense">
+            <AppBar position="static" className="appbar">
+                <Toolbar variant="dense" className='toolbar'>
                     <Box className='cursor'>
-                        <Typography variant="h5" style={{color: "#2a2a2a"}}>
+                        <Typography variant="h5" style={{color: "white"}}>
                             Hortykut 
                         </Typography>
                     </Box>
 
                     <Box display="flex" justifyContent="start">
                         <Box mx={1} className='cursor'>
-                            <Typography variant="h6" style={{color: "#2a2a2a"}}>
+                            <Typography variant="h6" style={{color: "white"}}>
                                 Venda
                             </Typography>
                         </Box>
                         <Box mx={1} className='cursor'>
-                            <Typography variant="h6" style={{color: "#2a2a2a"}}>
+                            <Typography variant="h6" style={{color: "white"}}>
                                 Compra
                             </Typography>
                         </Box>
                         <Box mx={1} className='cursor'>
-                            <Typography variant="h6" style={{color: "#2a2a2a"}}>
-                                transporte
+                            <Typography variant="h6" style={{color: "white"}}>
+                                Transporte
                             </Typography>
                         </Box>
                         <Box mx={1} className='cursor'>
-                            <Typography variant="h6" style={{color: "#2a2a2a"}}>
-                                cursos
+                            <Typography variant="h6" style={{color: "white"}}>
+                                Cursos
                             </Typography>
                         </Box>
                         <Box mx={1} className='cursor'>
-                            <Typography variant="h6" style={{color: "#2a2a2a"}}>
-                                planos
+                            <Typography variant="h6" style={{color: "white"}}>
+                                Planos
                             </Typography>
                         </Box>
                         <Box mx={1} className='cursor'>
-                            <Typography variant="h6" style={{color: "#2a2a2a"}}>
-                                Sobre nós
+                            <Typography variant="h6" style={{color: "white"}}>
+                                Sobre Nós
                             </Typography>
                         </Box>
                         <Box mx={1} className='cursor'>
-                            <Typography variant="h6" style={{color: "#2a2a2a"}}>
-                                suporte
+                            <Typography variant="h6" style={{color: "white"}}>
+                                Suporte
                             </Typography>
                         </Box>
                         <Box mx={1} className='cursor'>
-                            <Typography variant="h6" style={{color: "#2a2a2a"}}>
+                            <Typography variant="h6" style={{color: "white", background: "#ff7b00"}}>
                                 Faça Parte
                             </Typography>
                         </Box>
                         <Link to='/login' className='text-decorator-none'>
                             <Box mx={1} className='cursor'>
-                                <Typography variant="h6" style={{color: "#2a2a2a"}}>
-                                    logout
+                                <Typography variant="h6" style={{color: "white", background: "#464248"}}>
+                                    Logout
                                 </Typography>
-                            </Box>
-                            
+                            </Box>                            
                         </Link>
-                    </Box>
-                    
+                    </Box>                    
                 </Toolbar>
             </AppBar>
         </>
