389aa3b1a4f77dacf9fbf945dfdddfd6584e6959
Charles-Luchiari <charles99na@gmail.com>
2023-06-22 13:36:43 -0300

    Criando pagina servicos de mentoria e conexoes

---
+++ src/paginas/servicos/Servicos.tsx
@@ -0,0 +1,113 @@
+import React from 'react';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Typography from '@mui/material/Typography';
+import { CardActionArea } from '@mui/material';
+import Box from '@mui/material/Box';
+import Modal from '@mui/material/Modal';
+import './servicos.css'
+
+const style = {
+    position: 'absolute' as 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    width: 400,
+    bgcolor: 'background.paper',
+    border: '2px solid #000',
+    boxShadow: 24,
+    p: 4,
+};
+
+function Servicos() {
+
+    const [open, setOpen] = React.useState(false);
+    const handleOpen = () => setOpen(true);
+    const handleClose = () => setOpen(false);
+
+    return (
+
+        <>
+            <Box>
+                <CardContent>
+                    <Card sx={{ maxWidth: 345 }} >
+                        <CardActionArea onClick={handleOpen}>
+                            <CardMedia
+                                component="img"
+                                height="200"
+                                image="src/assets/imagens/mentoria.jpg"
+                                alt="foto da mentoria"
+                            />
+                            <CardContent>
+                                <Typography gutterBottom variant="h5" component="div">
+                                    MENTORIA
+                                </Typography>
+                                <Typography variant="body2" color="text.secondary">
+                                    CLIQUE PARA SABER DA MENTORIA
+                                </Typography>
+                            </CardContent>
+
+                        </CardActionArea>
+                    </Card>
+                </CardContent>
+                <Modal
+                    open={open}
+                    onClose={handleClose}
+                    aria-labelledby="modal-modal-title"
+                    aria-describedby="modal-modal-description"
+                >
+                    <Box sx={style}>
+                        <Typography id="modal-modal-title" variant="h6" component="h2">
+                            Você sabe o que é a mentoria?
+                        </Typography>
+                        <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+                            Descrição da mentoria
+                        </Typography>
+                    </Box>
+                </Modal>
+            </Box>
+
+            <Box>
+                <CardContent>
+                    <Card sx={{ maxWidth: 345 }} >
+                        <CardActionArea onClick={handleOpen}>
+                            <CardMedia
+                                component="img"
+                                height="200"
+                                image="src/assets/imagens/conexoes.jpg"
+                                alt="foto da mentoria"
+                            />
+                            <CardContent>
+                                <Typography gutterBottom variant="h5" component="div">
+                                    CONEXÕES
+                                </Typography>
+                                <Typography variant="body2" color="text.secondary">
+                                    CLIQUE PARA SABER DA CONEXÕES
+                                </Typography>
+                            </CardContent>
+
+                        </CardActionArea>
+                    </Card>
+                </CardContent>
+                <Modal
+                    open={open}
+                    onClose={handleClose}
+                    aria-labelledby="modal-modal-title"
+                    aria-describedby="modal-modal-description"
+                >
+                    <Box sx={style}>
+                        <Typography id="modal-modal-title" variant="h6" component="h2">
+                            Você sabe o que são conexões?
+                        </Typography>
+                        <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+                            Descrição das conexoes
+                        </Typography>
+                    </Box>
+                </Modal>
+            </Box>
+        </>
+    );
+}
+
+export default Servicos;
